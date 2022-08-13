70a4b1f7d1cb4abcb0973be47d1fad01dba85ddf
nidhalovic <nidhalovic90@gmail.com>
2022-08-13 04:40:08 +0100

    Lesson 11 Completed

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -2,6 +2,7 @@ const { render } = require('ejs')
 const express = require('express')
 const router = express.Router()
 const Event = require("../models/Event")
+const { check, validationResult } = require('express-validator')
 
 //route home events
 router.get('/', (req,res) => {
@@ -23,30 +24,49 @@ router.get('/', (req,res) => {
 })
 // create Event 
 router.get('/create' , (req,res) => {
-    res.render('event/create')
+    res.render('event/create' , {
+        errors:false
+    })
 })
 
-router.post('/create', (req,res)=> {
+router.post('/create',[
+    check('title').isLength({min :5}).withMessage('Title should be more than 5 caracters'),
+    check('description').isLength({min: 5}).withMessage('Description should be more than 5 char'),
+    check('location').isLength({min: 3}).withMessage('Location should be more than 5 char'),
+    check('date').isLength({min: 5}).withMessage('Date should valid Date'),
 
-    console.log(req.body)
-    let newEvent = new Event({
+], (req,res)=> {
+
+    const errors = validationResult(req)
+
+    if (! errors.isEmpty()){
+        //res.json(errors.array())
+        res.render('event/create', {
+            errors: errors.array()
+        })
+    }
+    else {
+       // res.json("ok")
+       let newEvent = new Event({
         title: req.body.title,
         description: req.body.description,
         date: req.body.date,
         location: req.body.location,
         created_at: Date.now()
     })
-    newEvent.save( (err) => {
-        if(!err){
-            console.log("added successfuly");
-            res.render('/events')
-        }
-        else {
-            console.log(err);
+
+    newEvent.save( (err)=> {
+        if(!err) {
+            console.log('event was added')
+            res.redirect('/events')
+        } else {
+            console.log(err)
         }
     })
-
+}
+   
 })
+ 
 //show single event
 router.get('/:id', (req,res) => {
     Event.findById((req.params.id), (err,eventResult)=> {
